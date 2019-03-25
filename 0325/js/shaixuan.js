9416c8368cb45799933825fd824347a52c1cc457
wuliTaoLeo <1435237272@qq.com>
2019-03-25 20:31:44 +0800

    0325

---
+++ 0325/js/shaixuan.js
@@ -0,0 +1,49 @@
+var pArr = [
+	{name : "小明",
+	 age : 18,
+	 sex : "男",
+	 jianjie : "你还得比较方便大家发表的就是封闭式的肌肤补水"
+	},
+	
+	{name : "小红",
+	 age : 18,
+	 sex : "女",
+	 jianjie : "第八节大脑的坎上看见发你的肌肤变得十分"
+	},
+	
+	
+	{name : "蔡徐坤",
+	 age : 30, 
+	 sex : "人妖",
+	 jianjie : "哦电脑都会比较奋笔疾书的方便大家方便地将发布的"
+	},
+	
+	{name : "小刚",
+	 age : 20,
+	 sex : "男",
+	 jianjie : "你打算今年比较方便的时间分别登上福布斯的积分"
+	},
+	
+	{name : "小花",
+	 age : 21,
+	 sex : "女",
+	 jianjie : "你还记得那个大明湖畔的夏雨荷吗？"
+	},
+	
+	{name : "吴亦凡",
+	 age : 18,
+	 sex : "人妖",
+	 jianjie : "老飞机到南方基本算法都是北方大部分回答是不分家的沙发"
+	},
+	
+	{name : "王娜",
+	 age : 18,
+	 sex : "女",
+	 jianjie : "人美话不多"
+	}
+]
+
+
+function listToArr(nodeList){
+				return Array.prototype.slice.call(nodeList, 0);
+			}
